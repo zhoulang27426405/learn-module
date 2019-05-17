419b7b99eaf16d824644e92d736f133d7f190ba0
zhoulang <zhoulang@zhuanzhuan.com>
2019-05-17 20:45:29 +0800

    exports用法补充

--- cjs/demo1/example.js
+++ cjs/demo1/example.js
@@ -7,3 +7,20 @@ module.exports = {
   x,
   add
 }
+
+/*
+ *可以使用exports简写module.exports
+ */
+// exports.x = x
+// exports.add = add
+
+/*
+ * 但是不能直接将exports变量指向一个值下述这种情况只能使用module.exports
+ */
+// module.exports = function() {
+//   console.log('hello')
+// }
+
+// exports = function() {
+//   console.log('hello')
+// }
--- cjs/demo1/index.js
+++ cjs/demo1/index.js
@@ -1,3 +1,3 @@
 const example = module.require('./example.js')
 
-console.log(example.add(1))
+console.log(example)
