c8ca3486ed687657c145719637dadd8a062f67f4
zhoulang <zhoulang@zhuanzhuan.com>
2019-07-31 23:23:25 +0800

    demo

--- esm/demo10/moduleA.js
+++ esm/demo10/moduleA.js
@@ -1,7 +1,8 @@
 import { bar } from './moduleB.js'
-export function foo() {
-  return 'foo'
-}
+export let foo = 'foo'
+// export function foo() {
+//   return 'foo'
+// }
 
 console.log("========moduleA===========")
 console.log(bar())
